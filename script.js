50046f762cdaeb5e35a866bc3785650a839b385d
Boris Traljic <btraljic@gmail.com>
2023-03-02 16:16:28 +0100

    main: card

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 //all jQuery in this file.
 
 $(document).ready(function () {
-  console.log("");
-
+  // --------------------------------------------------------------------------
+  // ----- Change header class on scroll
+  // --------------------------------------------------------------------------
   $(window).scroll(function () {
     if (window.pageYOffset > 400) {
       $("header").addClass("scrolled");
     } else {
       $("header").removeClass("scrolled");
     }
+  });
+
+  // --------------------------------------------------------------------------
+  // ----- Go to url from span
+  // --------------------------------------------------------------------------
+  $(".card").click(function (e) {
+    e.preventDefault();
+    window.location = $(this).attr("href");
   });
 });
